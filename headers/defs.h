4846914f04fa67db581f47a55059e3a9863f1a80
Henrique Mochida peixoto <hmochida@c1r10p1.42sp.org.br>
2022-06-16 10:32:50 -0300

    make git

--- headers/defs.h
+++ headers/defs.h
@@ -6,12 +6,22 @@
 /*   By: hmochida <hmochida@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/16 09:17:16 by hmochida          #+#    #+#             */
-/*   Updated: 2022/06/16 09:35:10 by hmochida         ###   ########.fr       */
+/*   Updated: 2022/06/16 10:32:04 by hmochida         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DEFS_H
 # define DEFS_H
+
+
+typedef struct s_stack
+{
+	int				num;
+	unsigned char	indexed;
+	struct s_stack	*next;
+	struct s_stack	*prev;
+} t_stack;
+
 
 
 
